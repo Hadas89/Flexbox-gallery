500fff2d0f229d44198e07cff03c72e365539849
Hadas89 <Hadas89@gmail.com>
2019-01-09 23:51:18 +0200

    animations complete

--- main.js
+++ main.js
@@ -2,23 +2,50 @@ $( document ).ready(function() {
 		console.log( "ready!" );
 });
 
-//, 'images/7.jpg', 'images/8.jpg', 'images/9.jpg', 'images/10.jpg', 'images/11.jpg', 'images/12.jpg'
 
-var createGallery = function () {
-	imgArray = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg','images/4.jpg', 'images/5.jpg','images/6.jpg']
+let expandImages = function () {
+console.log( "ready!" );
+	var images = document.getElementsByClassName('panel');
 
-	for (var i = 0; i < imgArray.length; i++) {
-		 var imgChild = document.createElement('img');
+	for (let i = 0; i < images.length; i++) {
+images[i].addEventListener('click', function () {
 
-		document.querySelector('.gallery').appendChild(imgChild);
-			imgChild.src = imgArray[i];
-			imgChild.addEventListener('click', function () {
-			 this.classList.toggle('expand');
+	if (this.classList.contains('animation-open')) {
+			this.classList.remove('animation-open');
+					this.classList.add('animation-close');
 
+	} else if (this.classList.contains('animation-close')) {
+	this.classList.remove('animation-close');
+		this.classList.add('animation-open');
 
-
-			})
-
+	} else {
+			this.classList.add('animation-open')
 	}
 
-}();
+	// if (this.children[0].classList.contains('animation-open')) {
+	// 		this.children[0].classList.remove('animation-open');
+	// 		this.children[0].classList.toggle('animation-close');
+	//
+	// } else {
+	// 	this.children[0].classList.toggle('animation-open')
+	//
+	// }
+
+
+
+	// this.children[0].classList.toggle('animation-open');
+	// this.children[1].classList.toggle('hiddentext-show');
+
+
+
+
+})
+	}
+}
+
+
+
+expandImages()
+
+
+//5-6/1 - continue this!text over background img
